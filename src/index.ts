95724f465d9694b3a36e4d4ec54ad66a4f15d9ff
Alroy Dsouza <alroy.d@meragi.com>
2023-04-19 13:02:54 +0530

    feat(image): added Image component #MER-273

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Button } from './components/Button'
 import { Loader } from './components/Loader'
 import { Space } from './components/Space'
 import { Text } from './components/Text'
+import { Image } from './components/Image'
 
-export { Button, Loader, Space, Text }
+export { Button, Loader, Space, Text, Image }
