2eefc9c86890bc7a20a407a5cef2b06235495000
Mayank Kushal <mayank.kushal@meragi.com>
2023-04-21 00:36:42 +0530

    fix: build issues

--- lib/components/Text/Text.d.ts
+++ lib/components/Text/Text.d.ts
@@ -2,8 +2,9 @@ import React from 'react';
 import './Text.scss';
 export interface TextProps {
     children: React.ReactNode;
-    variant?: 'body' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'body' | 'code' | 'error';
+    variant?: 'body' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'body' | 'code' | 'description';
     strong?: boolean;
+    type?: 'success' | 'danger' | 'warning' | 'info' | 'brand' | 'default';
     italic?: boolean;
     link?: boolean;
     href?: string;
